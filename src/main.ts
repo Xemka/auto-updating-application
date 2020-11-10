02474f567d350036977a8c769aeb734441ba7ed3
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-10 20:32:55 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+// import electron from 'electron';
+
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+const { autoUpdater } = require('electron-updater');
+const logger = require('electron-log');
+logger.transports.file.level = 'info';
+logger.transports.file.file = __dirname + '..' + 'log.log';
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+
+    });
+    mainWindow.loadURL(`${process.cwd()}/public/index.html`);
+
+    /**
+     * Auto-updater
+     */
+
+    /**
+     * Actions after a successful update download
+     */
+    autoUpdater.on('update-downloaded', (updateInfo: any) => {
+        logger.log('Update is ready: ', updateInfo);
+    });
+
+    /**
+     * Regularly checking for updates (current interval: 10 min)
+     */
+    setInterval(() => {
+        autoUpdater.checkForUpdates();
+    }, 10);
+
+});
